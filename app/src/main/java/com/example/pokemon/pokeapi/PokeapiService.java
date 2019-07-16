d7aa9478e299093786e18a5026e0bc4072cee14c
AlfredoMJ129 <AlfredoMJ129@gmail.com>
2019-07-16 16:44:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pokemon/pokeapi/PokeapiService.java
@@ -0,0 +1,14 @@
+package com.example.pokemon.pokeapi;
+
+import com.example.pokemon.models.PokemonRespuesta;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PokeapiService {
+
+    @GET("pokemon")
+    Call<PokemonRespuesta> obtenerListaPokemon(@Query("limit") int limit, @Query("offset") int offset);
+
+}
